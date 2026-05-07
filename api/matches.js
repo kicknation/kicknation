5045f73e4f3495687506c9cc04f0a3cadbdfb6a0
kicknation <kemar.dmatthews14@gmail.com>
2026-05-07 15:59:10 -0500

    Update matches.js

--- api/matches.js
+++ api/matches.js
@@ -1,1 +1,24 @@
 
+export default async function handler(req, res) {
+  try {
+
+    const response = await fetch("https://api.football-data.org/v4/matches", {
+      headers: {
+        "X-Auth-Token": process.env.FOOTBALL_API_KEY
+      }
+    });
+
+    const data = await response.json();
+
+    res.status(200).json({
+      success: true,
+      matches: data.matches
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err.message
+    });
+  }
+}
