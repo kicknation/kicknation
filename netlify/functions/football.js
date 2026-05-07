501f58dd34ad9bcfa63ceed4074585891cf2a370
kicknation <kemar.dmatthews14@gmail.com>
2026-05-07 12:52:50 -0500

    Create football.js

---
+++ netlify/functions/football.js
@@ -0,0 +1,22 @@
+exports.handler = async function () {
+  try {
+    const response = await fetch("https://api.football-data.org/v4/matches", {
+      headers: {
+        "X-Auth-Token": process.env.FOOTBALL_API_KEY
+      }
+    });
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Server error" })
+    };
+  }
+};
